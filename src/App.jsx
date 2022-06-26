9428af7279f7e8f4989c55f1e1ce7b03da4f12e5
e2f5db0 <sami.toiskallio@protonmail.com>
2022-06-26 14:19:07 +0300

    Break the build to get a discord notification

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
+// break the build to test the pipeline notification
+make the build break
+
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
   name,
